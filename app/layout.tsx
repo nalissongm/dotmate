720a30c99468495e54178b8b544df058170cb593
Nalisson Gomes <nalissongm.pro@gmail.com>
2024-03-07 00:20:06 -0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Toast from './toast';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'Next.js App Router + NextAuth + Tailwindd CSS',
   description:
     'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.'
 };
